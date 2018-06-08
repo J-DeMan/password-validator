2f1f291768459a3256c5643bab9692c953a298ee
jdeman <jerrett.deman@gmail.com>
2018-06-08 10:51:54 -0300

    Adds stage 1 validator unit tests

--- app/src/main/java/com/deman/jerrett/password_validator/Validator.java
+++ app/src/main/java/com/deman/jerrett/password_validator/Validator.java
@@ -1,7 +1,25 @@
 package com.deman.jerrett.password_validator;
 
+import java.util.HashMap;
+
+/**
+ * A class that contains methods for validating passwords
+ */
 public class Validator {
+    /**
+     * Validates that a password follows a set of rules
+     * @param password
+     * @return number of rules failed
+     */
     public static int validate(String password) {
         return 0;
+    }
+
+    public static boolean isNotCommon(String password) {
+        return false;
+    }
+
+    public static boolean isMinChar(String password) {
+        return false;
     }
 }
--- app/src/test/java/com/deman/jerrett/password_validator/ValidatorUnitTest.java
+++ app/src/test/java/com/deman/jerrett/password_validator/ValidatorUnitTest.java
@@ -5,8 +5,48 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ValidatorUnitTest {
+    /**
+     * Checks that common passwords fail
+     */
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void isCommon() {
+        boolean result = Validator.isNotCommon("password");
+        assertFalse(result);
+    }
+
+    /**
+     * Checks that non-common passwords pass
+     */
+    @Test
+    public void isNotCommon() {
+        boolean result = Validator.isNotCommon("notCommon");
+        assertTrue(result);
+    }
+
+    /**
+     * Checks that passwords above 8 chars pass
+     */
+    @Test
+    public void isAboveMinChar() {
+        boolean result = Validator.isMinChar("above8Chars");
+        assertTrue(result);
+    }
+
+    /**
+     * Checks that passwords that are 8 chars pass
+     */
+    @Test
+    public void isMinChar() {
+        boolean result = Validator.isMinChar("12345678");
+        assertTrue(result);
+    }
+
+    /**
+     * Checks that passwords under 8 chars fail
+     */
+    @Test
+    public void underMinChar() {
+        boolean result = Validator.isMinChar("1234567");
+        assertFalse(result);
     }
 }

2c03da32dabc5b05008719063e518671596bb6f0
Jerrett DeMan <jerrett.deman@gmail.com>
2018-06-09 15:53:45 -0300

    Adds UI validation button event handler

--- app/src/main/java/com/deman/jerrett/password_validator/MainActivity.java
+++ app/src/main/java/com/deman/jerrett/password_validator/MainActivity.java
@@ -13,6 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button validateButton = (Button)findViewById(R.id.validateButton);
+        final EditText inputField = (EditText)findViewById(R.id.inputField);
+        final TextView resultField = (TextView)findViewById(R.id.resultField);
+
+        validateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String input = inputField.getText().toString();
+
+                if (Validator.validate(input) > 0) {
+                    resultField.setText("Password is not string enough.");
+                }
+                else {
+                    resultField.setText("Password is strong.");
+                }
+            }
+        });
     }
 
 
